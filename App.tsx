22b2c8ba4e8dae3f41e92192a20d6205d8398e3c
Mannonen Lukas <lukas.mannonen@hotmail.fr>
2022-05-23 14:38:20 +0200

    exercice3

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import TitleText from './component/TitleText';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TitleText title='Le titre de mon article' text='Le texte de mon article' />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
